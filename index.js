d63f04da802017e9dc5ffbed0810898cebed0a86
OzzyHunter322 <126529454+OzzyHunter322@users.noreply.github.com>
2023-02-27 21:39:02 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').addEventListener('click' , function(){
+    document.getElementById("texto").style.display = 'block';
+    document.getElementById("boton").style.display = 'none';
+})
